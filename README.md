f9628285fd416ede42c68eac71421aa0d84c107c
FKSAxell <kevin.concha.regatto@outlook.com>
2019-12-11 20:04:39 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# proyectodaw2019
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/proyectodaw2019)
---
+++ src/app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { NavComponent } from './shared/nav/nav.component';
+
+@NgModule({
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, NavComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
