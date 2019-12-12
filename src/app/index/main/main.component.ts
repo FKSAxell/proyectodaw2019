55f9625753af6122a9ba816f49e50aeea990f9ab
FKSAxell <kevin.concha.regatto@outlook.com>
2019-12-12 00:22:13 -0500

    1 file modified

--- src/app/index/main/main.component.ts
+++ src/app/index/main/main.component.ts
@@ -10,6 +10,7 @@ export class MainComponent implements OnInit {
   
   constructor() { 
    this.iniciarlizarCarrera()
+   this.inicializarMaterias()
   }
   
   ngOnInit() {
@@ -60,20 +61,53 @@ export class MainComponent implements OnInit {
     .then( respuesta => respuesta.json())
     .then( data => {
       let arreglo = data.Carreras;
+      let cont=0
+      let divMaster= document.getElementById("main")
       for (let objeto of arreglo) {
         let carrera= objeto["carrera"]
         let codigo= objeto["codigo"]
         let materias= objeto["materias"]
 
         let section=document.createElement("section")
-        let divCon,divCol12,divRow,divCol122=document.createElement("div")
+        let divCon=document.createElement("div")
+        let divCol12=document.createElement("div")
+        let divRow=document.createElement("div")
+        let divRow2=document.createElement("div")
+        let divCol122=document.createElement("div")
+        if(cont%2==0){
+          section.setAttribute("class","page-section bg-primary")
+        }else{
+          
+          section.setAttribute("class","page-section")
+        }
+        
+        divRow.setAttribute("class","row justify-content-center")
+
+        divCon.setAttribute("class","container")
+
+        divRow2.setAttribute("class","row justify-content-center")
+
+        divCol12.setAttribute("class","col-lg-12 text-center")
+        divCol12.setAttribute("id",codigo)
+        let h2 =document.createElement("h2")
+        let txth2 = document.createTextNode(carrera)
+        
+        h2.appendChild(txth2)
+        h2.setAttribute("class","text-black mt-0")
         let hr =document.createElement("hr")
+        hr.setAttribute("class","divider light my-4")
         let p =document.createElement("p")
+        
+        p.setAttribute("class","text-gray-50 mb-4")
+        divRow2.setAttribute("class","row justify-content-center")
+        divCol122.setAttribute("class","col-lg-12")
         let ul=document.createElement("ul")
+        
+        ul.setAttribute("class","row")
         for(let dato of materias){
           let materia = dato["materia"]
           let tag =dato["tag"]
-
+          
           let li=document.createElement("li")
           li.setAttribute("class","navbar-nav ml-auto my-2 col-lg-6")
           let a=document.createElement("a")
@@ -81,13 +115,25 @@ export class MainComponent implements OnInit {
           a.setAttribute("href",materia)
           let txt = document.createTextNode(materia)
           a.appendChild(txt)
-        
           li.appendChild(a)
           ul.appendChild(li)
+          
 
         }
         
-
+        divCol122.appendChild(ul)
+        divRow2.appendChild(divCol122)
+        p.appendChild(divRow2)
+        divCol12.appendChild(h2)
+        divCol12.appendChild(hr)
+        divCol12.appendChild(p)
+        divRow.appendChild(divCol12)
+        divCon.appendChild(divRow)
+        section.appendChild(divCon)
+        
+        divMaster.appendChild(section)
+        cont++;
+        
 
       }
     })
